2556904ec447a2d3062f93243fe96a1aeaeb9203
aivarasmink <aivarasmink@gmail.com>
2024-02-14 13:03:38 +0200

    sukurta tasks_detail ir task_list

--- tasket_aivi/tasks_aivi/urls.py
+++ tasket_aivi/tasks_aivi/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
+    path('tasks/', views.task_list, name='task_list'),
+    path('tasks/<int:pk>/', views.task_detail, name='task_detail'),
+    path('tasks/<int:pk>/done/', views.task_done, name='task_done'),
 ]
