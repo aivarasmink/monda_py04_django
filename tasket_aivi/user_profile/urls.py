e88db08ec347fb50a48ed47d2a244e9d2c28913b
aivarasmink <aivarasmink@gmail.com>
2024-02-22 11:34:24 +0200

    profile redagavimo forma sukurta

--- tasket_aivi/user_profile/urls.py
+++ tasket_aivi/user_profile/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('detail/', views.user_detail, name='user_detail_current'),
-    path('detail/<stug:username>/', views.user_detail, name='user_detail'),
+    path('detail/<str:username>/', views.user_detail, name='user_detail'),
+    path('edit/', views.user_update, name='user_update'),
 ]
-
--- tasket_aivi/user_profile/views.py
+++ tasket_aivi/user_profile/views.py
@@ -13,8 +13,8 @@ def signup(request: HttpRequest) -> HttpResponse:
         form = forms.CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, _("Thank you! You can log in now with your credentials."))
-            return redirect("login")
+            messages.success(request, _("Thank you! You can now log in with your credentials."))
+            return redirect('login')
     else:
         form = forms.CreateUserForm()
     return render(request, 'user_profile/signup.html', {
@@ -29,4 +29,22 @@ def user_detail(request: HttpRequest, username: str | None = None) -> HttpRespon
         user = request.user
     return render(request, 'user_profile/user_detail.html', {
         'object': user,
+    })
+
+@login_required
+def user_update(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form_user = forms.UserForm(request.POST, instance=request.user)
+        form_profile = forms.ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if form_user.is_valid() and form_profile.is_valid():
+            form_user.save()
+            form_profile.save()
+            messages.success(request, _("profile edited successfully").capitalize())
+            return redirect('user_detail_current')
+    else:
+        form_user = forms.UserForm(instance=request.user)
+        form_profile = forms.ProfileForm(instance=request.user.profile)
+    return render(request, 'user_profile/user_update.html', {
+        'form_user': form_user,
+        'form_profile': form_profile,
     })
