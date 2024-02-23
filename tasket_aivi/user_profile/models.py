329bf3ef203033b04194fb96a79396fbe3b50e65
aivarasmink <aivarasmink@gmail.com>
2024-02-23 07:55:46 +0200

    nieko nekeiciau

--- tasket_aivi/user_profile/models.py
+++ tasket_aivi/user_profile/models.py
@@ -3,7 +3,8 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.utils.translation import gettext as _
-from PIL import Image
+from PIL import Image 
+
 
 
 class Profile(models.Model):
